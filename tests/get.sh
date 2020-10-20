4dcc71becdbbc0cf91b35a903b1080c8957019f6
Michael McDermott <michael.mcdermott@kitware.com>
2020-10-20 19:15:34 -0400

    added docker, travis, tests, updated flags

---
+++ tests/get.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+errcho() {
+    (>&2 echo -e "\e[31m$1\e[0m")
+}
+
+
+for url in 'http://api.ipify.org' 'http://google.com' 'google.com' 'google.com:80'; do
+    out=$(justget "$url")
+    if [[ $? -ne 0 ]]; then
+      FAILED=1
+      errcho "Failed url test: ${url}"
+    fi
+
+done
+
+if [[ -n "$FAILED" ]]; then
+  echo "Failed one or more tests"
+  exit 1
+fi
+
+echo "PASS!"
